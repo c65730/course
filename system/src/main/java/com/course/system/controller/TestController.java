425786db1c7259185c08bc51f51815deb50990ce
c65730 <c65730@126.com>
2020-10-29 11:10:01 +0800

    1. mybatis使用示例：http:/127.0.0.1:9001/system/test 2. 增加三层结构，入口controller，逻辑service，持久mapper

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/test")
-    public String test(){
+    @Resource
+    private TestService testService;
 
-        return "success";
+    @RequestMapping("/test")
+    public List<Test> test(){
+
+        List<Test> list = testService.list();
+        return list;
     }
 }
