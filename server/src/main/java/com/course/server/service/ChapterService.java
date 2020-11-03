acceb7b48043f4b7a87648446d0ee118f052632d
c65730 <c65730@126.com>
2020-11-03 15:41:19 +0800

    1. 集成分页插件pagehelper，注意页码从1开始

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -5,9 +5,9 @@ import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-import sun.text.resources.iw.FormatData_iw_IL;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -21,6 +21,7 @@ public class ChapterService {
 
     public List<ChapterDto> list(){
 
+        PageHelper.startPage(10,1);
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtoList = new ArrayList<>();
