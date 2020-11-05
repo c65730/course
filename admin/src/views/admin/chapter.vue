5cb4bdd51c34e7bf215bdee9bf37f17129d2592b
c65730 <c65730@126.com>
2020-11-05 14:22:01 +0800

    1. 增加Loading组件

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -105,15 +105,16 @@
       },
       list(page){
         let _this = this;
+        Loading.show();
         _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",{
           page: page,
           size: _this.$refs.pagination.size
         }).then((response)=>{
+          Loading.hide();
           console.log("查询大章列表结果：", response);
           let resp = response.data;
           _this.chapters = resp.content.list;
           _this.$refs.pagination.render(page, resp.content.total);
-
         })
       },
       del(id){
