da0b6f8651acdf287e2b87714e0793dcb8f23cc0
c65730 <c65730@126.com>
2020-11-11 11:09:17 +0800

    1. zTree数据改为分类所有记录

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -183,12 +183,13 @@
         COURSE_LEVEL: COURSE_LEVEL,
         COURSE_CHARGE: COURSE_CHARGE,
         COURSE_STATUS: COURSE_STATUS,
+        categorys: [],
       }
     },
     mounted: function() {
       let _this = this;
       _this.$refs.pagination.size = 5;
-      _this.initTree();
+      _this.allCategory();
       _this.list(1);
       // sidebar激活样式方法一
       // this.$parent.activeSidebar("business-course-sidebar");
@@ -297,23 +298,35 @@
         _this.$router.push("/business/content");
       },
 
+      allCategory() {
+        let _this = this;
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/category/all').then((response)=>{
+          Loading.hide();
+          let resp = response.data;
+          _this.categorys = resp.content;
+
+          _this.initTree();
+        })
+      },
+
       initTree(){
+        let _this = this;
         let setting = {
           check: {
             enable: true
           },
           data: {
             simpleData: {
+              idKey: "id",
+              pIdKey: "parent",
+              rootPId: "00000000",
               enable: true
             }
           }
         };
 
-        let zNodes =[
-          {id:1,pId:0,name:"随意勾选 1",open:true},
-          {id:11,pId:1,name:"随意勾选 1-1",open:true},
-          {id:111,pId:11,name:"随意勾选 1-1-1",open:true}
-        ];
+        let zNodes = _this.categorys;
 
         $.fn.zTree.init($("#tree"),setting,zNodes);
       }
