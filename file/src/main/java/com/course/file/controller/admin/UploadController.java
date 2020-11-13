cf6ebb21d4a8e255d8e7fa5da6b5b9c0da36b6e6
c65730 <c65730@126.com>
2020-11-13 10:53:25 +0800

    1. 文件上传功能开发：实时显示与保存

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -37,7 +37,7 @@ public class UploadController {
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
 
-
+        responseDto.setContent("http://127.0.0.1:9000/file/f/teacher/" +key+ "-"+fileName);
         return responseDto;
     }
 }
